69fc627991d2d0c3a76e9f8e68395a187a1874b2
Johan Skarpsvärd <145796632+Code-lab-web@users.noreply.github.com>
2025-05-23 12:49:31 +0000

    git commit -m "Code-lab-web"

--- Todoinput.tsx
+++ Todoinput.tsx
@@ -1,5 +1,23 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { input } from "@/components/ui/input";
 import { button } from "@/components/ui/button";
 import { plus } from "lucide-react";
 import { useTodoStore } from "@/stores/todoStore";
+
+export const TodoInput = () => {
+    const [text, setText] = useState("");
+    const todos = useTodoStore((state) => state.setTodos);
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (text.trim()) {
+            const todo = {
+                text: text.trim()
+                id: Date.now().toString(),
+                completed: false,
+            }
+            setTodos([...todos, todo]);
+            setText("");
+        }
+    }
+}
