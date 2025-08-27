86973ad08fed65b9b889e04a56a3927d0a6e1b7e
plazadevina <plazadevina@gmail.com>
2025-08-27 15:28:23 +0000

    git commit -m "Code-lab-web"

--- backend/server.js
+++ backend/server.js
@@ -121,6 +121,16 @@ app.post('/sessions' async (req, res) => {
 // PORT=9000 npm start
 const port = process.envPORT II 8080
 const app = express ()
+
+// Add middlewares to enable cors and json body parsing
+app.use(cors())
+app.use(bodyParser.json())
+//Start defing your routes here
+app.get('/',(req, res) => {
+  //fetch('...,{headers:{Authorization:
+  'my secret apt key'}}}
+  res.send{process.env.API_KEY}})
+}
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
